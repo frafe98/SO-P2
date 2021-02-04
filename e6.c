d00851e6c147316c9a3f6dfda10f4c1dd98562d2
FRANCESC FERRER <francesc.ferrer01@estudiant.upf.edu>
2021-02-04 17:05:03 +0000

    E6 funciona todo menos 3 y 4

--- e6.c
+++ e6.c
@@ -8,16 +8,6 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-#include <sys/types.h> /* pid_t */
-#include <sys/wait.h>  /* wait */
-#include <stdio.h>     /* printf */
-#include <unistd.h>    /* _exit, fork, execvp */
-#include "myutils.c"
-#include "myutils.h"
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-
 
 
 /************************* Definiciones Globales ********************************/
@@ -33,6 +23,17 @@ typedef struct cmd_struct {//Estructura en la que guardamos los comandos y los a
 
 
 /******************************* Funciones **************************************/
+
+int myWait(){
+  int status, res;
+  res = wait(&status);
+    if ( WIFEXITED(status) ) {   
+      const int es = WEXITSTATUS(status);
+      if(es == 1) printf("Ha habido un problema al ejecutar el comando\n");
+      else if (es == 2) printf("Ha habido un problema al llamar a la función executeCmd");
+    }
+  return res;
+}
 
 int saveCmds(int file, cmd_type *cmd){
   
@@ -87,25 +88,21 @@ int saveCmds(int file, cmd_type *cmd){
 //Función que imprime todos los comandos guardados en memoria junto con sus respectivos argumentos
 //Recibe una estructura del tipo cmd_type que contiene toda la info de cada comando, y un entero que indica el número de comandos almacenados en la estructura.
 
+
 void printSavedCmds(cmd_type *cmd, int ncmds){
-  char asper = '&';
-  
+
   for(int i = 0; i < ncmds; i++){
     
     printf("\n%d) cmd: %s", i+1, cmd[i].cmd);
     if (cmd[i].nargs > 0){      
       for(int j = 0; j < cmd[i].nargs; j++){
-        char * comp = strchr(cmd[i-1].args[j], asper);
-        if (((j == cmd[i-1].nargs) && comp != NULL)){ //Si no nos encontramos en el último argumento y el argumento no es un & entonces imprimimos por pantalla
+        if (!((j == cmd[i].nargs-1) && (strchr("&", *cmd[i].args[j]) != NULL))){ //Si no nos encontramos en el último argumento y el argumento no es un & entonces imprimimos por pantalla
           printf(" argv[%d]: %s", j+1, cmd[i].args[j]);
-        }  else{
-          printf("Y\n");
-        }      
+        }   
       }
     }
   }
 }
-
 //Función que imprime solamente el comando que le indiquemos mediante un entero.
 void printSomeCmd(cmd_type *cmd, int nCmd){
   
@@ -121,32 +118,56 @@ void printSomeCmd(cmd_type *cmd, int nCmd){
 }
 
 //Función que ejecutará un comando por terminal. Creara un proceso hijo y el será el que ejecute el comando
+
+
 void executeCmd(cmd_type *cmd, int nCmd){
   
   char *argv[10];
   
   pid_t pid;
   pid = fork();
-
-  if(pid == 0){        
-    //memcpy(argv[0], cmd[nCmd].cmd, txtSize);    
-    argv[0] = cmd[nCmd].cmd;
-    
-    if (cmd[nCmd].nargs > 0){      
-      for (int i = 1; i <= cmd[nCmd].nargs; i++){
-        //memcpy(&argv[i], cmd[nCmd].args[i-1], txtSize);        
-        argv[i] = cmd[nCmd].args[i-1];        
-      }     
-    }
-
+       
+  //memcpy(argv[0], cmd[nCmd].cmd, txtSize);    
+  argv[0] = cmd[nCmd].cmd;
+  
+  if (cmd[nCmd].nargs > 0){
+    if(strchr("&", *cmd[nCmd].args[cmd[nCmd].nargs-1]) != NULL){
+      //Hacer ejecucion concurrida
+      for (int i = 0; i < cmd[nCmd].nargs-1; i++){
+        argv[i+1] = cmd[nCmd].args[i];
+      }
+      argv[cmd[nCmd].nargs + 1] = NULL;//Para indicar donde acaba la parte útil del array
+      //Falta la movida de que sea concurrido
+      if(pid == 0){   
+        execvp(argv[0], &argv[0]);
+        exit(3);
+      }
+    }else{
+      //Hacer ejecución normal (no concurrida)
+      for (int i = 0; i < cmd[nCmd].nargs; i++){
+        argv[i+1] = cmd[nCmd].args[i];
+      }
+      argv[cmd[nCmd].nargs + 1] = NULL;//Para indicar donde acaba la parte útil del array
+      if(pid == 0){   
+        execvp(argv[0], &argv[0]);
+      }      
+      myWait();//wait con deteccion de errores (hace un print en caso de error dando algo de info)
+    }      
+  }else{
     argv[cmd[nCmd].nargs + 1] = NULL;//Para indicar donde acaba la parte útil del array
-    
-    printf("\n");
-    execvp(argv[0], &argv[0]);
+    if(pid == 0){
+      execvp(argv[0], &argv[0]);
+      _exit(1);
+    }    
+    myWait();
+  }     
+  
+  printf("\n");
+  
+  if(pid == 0){//Por si nos falla el execvp de una llamada concurrida aquí llegará un proceso hijo
     _exit(1);//Error de ejecución del comando
-  }
-
-
+  }  
+  
 }
 
 int main(int argc, char* argv[]){
@@ -176,18 +197,14 @@ int main(int argc, char* argv[]){
       printf("Input inválido, prueba otra vez\n");
     }else{
       //printSomeCmd(cmds, num);
-      executeCmd(cmds, num-1);
-      
-      int status;
-      wait(&status);
-      if ( WIFEXITED(status) ) {   
-        const int es = WEXITSTATUS(status);
-        if(es == 1) printf("Ha habido un problema al ejecutar el comando\n");
-        else if (es == 2) printf("Ha habido un problema al llamar a la función executeCmd");
-      }
+      executeCmd(cmds, num-1);      
     }
+     
   }
 
+  while(myWait() > 0); 
+  
+  
   //printSavedCmds(cmds, ncmds);  
 
   close(file);
